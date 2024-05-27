387f5b06f65581e97632db5fb9d264d564680cce
Nihat Yüksel <yukselnihat7@gmail.com>
2024-05-27 23:25:44 +0300

    atmV2

--- src/main/java/freetasks/AtmApp.java
+++ src/main/java/freetasks/AtmApp.java
@@ -1,2 +1,73 @@
-package freetasks;public class AtmApp {
-}
+package freetasks;
+
+import java.util.Scanner;
+
+public class AtmApp {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        //Checking the name and surname
+
+        System.out.println("Please enter your name: ");
+        String name = input.nextLine().trim();
+
+        System.out.println("Please enter your surname: ");
+        String surname = input.nextLine();
+
+        String fullName = name + " " + surname;
+        String upperFullname = fullName.toUpperCase();
+        System.out.println("Welcome to your account \n" + upperFullname);
+
+        System.out.println("Please enter your new password: ");
+        String newpwd = input.nextLine();
+        String lowerNewpwd = newpwd.toLowerCase();
+        //Buraya kadar doğru çalışıyor
+        //-----------------------------------------------------
+
+        String lowName = name.toLowerCase().trim();
+        String[] splitNames = lowName.split(" ");
+        String name1 = splitNames[0];
+        String name2 = splitNames[1];
+        String lowSurname = surname.toLowerCase();
+        if (lowerNewpwd.contains(name1)){
+            System.out.println("Your password should not contain your name or surname");
+        } else if (lowerNewpwd.contains(name2)) {
+            System.out.println("Your password should not contain your name or surname");
+        } else if (lowerNewpwd.contains(lowSurname)) {
+            System.out.println("Your password should not contain your name or surname");
+        }else {
+
+        //Checking password
+
+            System.out.println("Please enter your password to verify: ");
+        String userpwd = input.nextLine();
+
+
+        int userTry = 1;
+        int mainAccountBalance = 10000;
+            while (userTry < 4){
+
+            if (!userpwd.equals(newpwd)) {
+                System.out.println("Password invalid. Please try again.");
+                userTry++;
+                return;
+            } else {
+
+                System.out.println("Main Account Balance: " + mainAccountBalance + "$");
+                break;
+            }}
+
+        System.out.println("Please enter the amount of money you want to withdraw: ");
+        int withdraw = input.nextInt();
+
+        if ((withdraw<=mainAccountBalance)&&(withdraw>0)) {
+            int withdrawn = mainAccountBalance - withdraw;
+            System.out.println("You withdraw : " + withdraw + "\n" + withdrawn + "$ has left");
+        }else {
+            System.out.println("Amount of money you want to withdraw should be above 0 and under your total Main Account Balance");
+        }
+        }
+    }
+        }
+
+
